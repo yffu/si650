f539aa33e0ea43d58bd004dd220f7dde5dcd4229
yffu <yuanfangfu8910@gmail.com>
2015-01-15 00:33:26 -0500

    third question, adapting parser from online

--- src/ch01/ex0102.py
+++ src/ch01/ex0102.py
@@ -6,19 +6,22 @@ Created on Jan 14, 2015
 
 import ex0101
 
-
-matrix = dict();
-result = ex0101.invert('../../data/ch01/collection02');
-
-docs = result[0];
-words = result[1];
-
-print docs 
-print words
-
-for k in words:
-    posting = words[k];
-    matrix[k]=[1 if (n in posting) else 0 for n in xrange(len(docs))]
+def invert_matrix(filename):
+    matrix = dict();
+    result = ex0101.invert(filename);
     
-for k in matrix:
-    print matrix[k], k;
+    docs = result[0];
+    words = result[1];
+    
+    print docs 
+    print words
+    
+    for k in words:
+        posting = words[k];
+        matrix[k]=[1 if (n in posting) else 0 for n in xrange(len(docs))]
+        
+    for k in matrix:
+        print matrix[k], k;
+    return [list, matrix];
+
+invert_matrix('../../data/ch01/collection02');
