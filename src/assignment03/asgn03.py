4d2df3bac9f399d413c8ef14555bd0ad0817ccdd
yffu <yuanfangfu8910@gmail.com>
2015-03-28 15:03:37 -0400

    new stuff for assignment 3

--- src/assignment03/asgn03.py
+++ src/assignment03/asgn03.py
@@ -3,3 +3,16 @@ Created on Mar 28, 2015
 
 @author: Yuan-Fang
 '''
+
+import re
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+
+with open('data/train.csv', 'r') as hand:
+    for line in hand:
+        match=re.search("^(\d+),(.+)$",line )
+        if (match):
+            print match.group(1), match.group(2)
+            tokens=word_ 
+        
+    
